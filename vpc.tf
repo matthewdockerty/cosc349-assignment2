06486e0c5a52f72795985250c9dcbcfb5cd05f3d
Matthew Dockerty <dockertyml@gmail.com>
2019-10-04 17:53:00 +1300

    Configure VPC security group ingress ports

--- vpc.tf
+++ vpc.tf
@@ -8,32 +8,32 @@ resource "aws_security_group" "app_servers" {
   vpc_id      = "${aws_vpc.cosc349_vpc.id}"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+      from_port = 443 
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-#   ingress {
-#     from_port   = 27017
-#     to_port     = 27017
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port   = 3000
-#     to_port     = 3000
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
